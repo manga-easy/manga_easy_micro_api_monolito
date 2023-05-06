cf502910469d50c88311d79c2f201d6d5a330b83
lucas <lucas.cardo123@hotmail.com>
2023-05-06 17:44:23 -0400

    add private id in Users Achievement Entity

--- src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/users/entities/UsersAchievementsEntity.kt
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/users/entities/UsersAchievementsEntity.kt
@@ -9,7 +9,7 @@ class UsersAchievementsEntity
  {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var _id: Long? = null
+    private var _id: Long? = null
     var _uid: String? = null
     var idemblema: String? = null
     var timecria: Long? = null
