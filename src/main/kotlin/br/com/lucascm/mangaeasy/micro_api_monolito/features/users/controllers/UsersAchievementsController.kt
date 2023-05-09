c05f979978b577d36181ea4fb847e72df0507c00
lucas <lucas.cardo123@hotmail.com>
2023-05-08 21:24:52 -0400

    Implement VerifyUserIdPermissionService int UsersAchievements

--- src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/users/controllers/UsersAchievementsController.kt
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/users/controllers/UsersAchievementsController.kt
@@ -2,21 +2,28 @@ package br.com.lucascm.mangaeasy.micro_api_monolito.features.users.controllers
 
 import br.com.lucascm.mangaeasy.micro_api_monolito.core.entities.ResultEntity
 import br.com.lucascm.mangaeasy.micro_api_monolito.core.entities.StatusResultEnum
+import br.com.lucascm.mangaeasy.micro_api_monolito.core.service.VerifyUserIdPermissionService
 import br.com.lucascm.mangaeasy.micro_api_monolito.features.achievements.repositories.AchievementsRepository
 import br.com.lucascm.mangaeasy.micro_api_monolito.features.users.entities.UsersAchievementsEntity
 import br.com.lucascm.mangaeasy.micro_api_monolito.features.users.repositories.UsersAchievementsRepository
 import com.fasterxml.jackson.annotation.ObjectIdGenerators.UUIDGenerator
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.*
 import java.util.Date
 
 @RestController
 @RequestMapping("/v1/users")
-class UsersAchievementsController(@Autowired val repository: UsersAchievementsRepository, @Autowired val achievementsRepository: AchievementsRepository) {
+class UsersAchievementsController(@Autowired val repository: UsersAchievementsRepository,
+                                  @Autowired val achievementsRepository: AchievementsRepository,
+                                  @Autowired val verifyUserIdPermissionService: VerifyUserIdPermissionService) {
     @GetMapping("/{uid}/achievements")
     @ResponseBody
-    fun listAchievements(@PathVariable uid: String, @RequestParam idEmblema: String?) : ResultEntity<UsersAchievementsEntity> {
+    fun listAchievements(@PathVariable uid: String,
+                         @RequestParam idEmblema: String?,
+                         authentication: Authentication) : ResultEntity<UsersAchievementsEntity> {
         try {
+            verifyUserIdPermissionService.get(authentication, uid);
             val result: List<UsersAchievementsEntity> = if (idEmblema == null){
                 repository.findAllByUserid(uid)
             }else{
@@ -40,8 +47,11 @@ class UsersAchievementsController(@Autowired val repository: UsersAchievementsRe
 
     @PostMapping("/{uid}/achievements/acquire")
     @ResponseBody
-    fun acquire(@PathVariable uid: String, @RequestBody body: UsersAchievementsEntity) : ResultEntity<UsersAchievementsEntity> {
+    fun acquire(@PathVariable uid: String,
+                @RequestBody body: UsersAchievementsEntity,
+                authentication: Authentication) : ResultEntity<UsersAchievementsEntity> {
         try {
+            verifyUserIdPermissionService.get(authentication, uid);
             val resultEmblema =  achievementsRepository.findAllByUid(body.idemblema!!)
             if (resultEmblema.isEmpty()){
                 throw Exception("Emblema não encontrado")
