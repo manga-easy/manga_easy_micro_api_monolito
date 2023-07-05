aff55624232a7ab807728db608fd4cd84208ae09
lucas <lucas.cardo123@hotmail.com>
2023-07-05 16:02:33 -0400

    Add attributes artistid and artistname

--- src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/recommendations/repositories/RecommendationsRepository.kt
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/recommendations/repositories/RecommendationsRepository.kt
@@ -2,9 +2,10 @@ package br.com.lucascm.mangaeasy.micro_api_monolito.features.recommendations.rep
 
 import br.com.lucascm.mangaeasy.micro_api_monolito.features.recommendations.entities.RecommendationsEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
 
-
+@Repository
 interface RecommendationsRepository : JpaRepository<RecommendationsEntity, Long> {
     fun findTop5ByOrderByUpdatedatDesc(): List<RecommendationsEntity>
     fun findAllByOrderByUpdatedatDesc(): List<RecommendationsEntity>
