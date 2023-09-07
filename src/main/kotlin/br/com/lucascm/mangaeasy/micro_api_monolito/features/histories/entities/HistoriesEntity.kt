6c34348b51b344e4a14611eb8ec507e7a652d8ff
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-09-07 17:08:58 -0300

    update HistoriesEntity

--- src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/histories/entities/HistoriesEntity.kt
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/histories/entities/HistoriesEntity.kt
@@ -1,25 +1,30 @@
 package br.com.lucascm.mangaeasy.micro_api_monolito.features.histories.entities
 
+import br.com.lucascm.mangaeasy.micro_api_monolito.features.mangas.entities.ChapterEntity
+import br.com.lucascm.mangaeasy.micro_api_monolito.features.mangas.entities.MangaEntity
 import jakarta.persistence.*
 import java.io.Serializable
 
 @Entity
-@Table(name = "NA")
-class HistoriesEntity: Serializable {
+@Table(name = "_1_database_1_collection_6")
+data class HistoriesEntity(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "_id")
+        private val id: Long? = null,
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private var _id: Long? = null
+        @Column(name = "_uid")
+        val uid: String? = null,
 
-    @Column(nullable = false)
-    var idUser: String? = null
-        private set
-
-    @Column(nullable = true)
-    var uniqueId: String? = null
-        private set
-
-    @Column(nullable = false)
-    var orderUpdate: Boolean? = null
-        private set
-}
+        val uniqueid: String,
+        val manga: MangaEntity,
+        val idUser: String,
+        @Deprecated(message = "User currentChapter 0.13.0 -> 0.15.0")
+        val capAtual: ChapterEntity?,
+        var currentChapter: String?,
+        val updatedAt: Int,
+        val createdAt: Int,
+        val isDeleted: Boolean,
+        val chapterLidos: List<String>,
+        val isSync: Boolean
+)
