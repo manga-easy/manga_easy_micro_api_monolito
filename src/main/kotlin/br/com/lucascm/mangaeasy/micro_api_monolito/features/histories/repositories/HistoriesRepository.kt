f31471fe3277561ad097794b4d839bc24545840e
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-09-09 01:57:02 -0300

    add findByIduserAndUniqueid and findByIduser

--- src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/histories/repositories/HistoriesRepository.kt
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/histories/repositories/HistoriesRepository.kt
@@ -1,16 +1,13 @@
+package br.com.lucascm.mangaeasy.micro_api_monolito.features.histories.repositories
 import br.com.lucascm.mangaeasy.micro_api_monolito.features.histories.entities.HistoriesEntity
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
 
-interface HistoriesRepository : JpaRepository<HistoriesEntity?, Long?> {
-    @Query("SELECT h FROM HistoriesEntity h WHERE h.idUser = :idUser " +
-            "AND h.uniqueid = :uniqueid  " +
-            "ORDER BY h.updatedAt DESC")
-    fun findHistoricoWithFilter(
-            @Param("idUser") idUser: String,
-            @Param("uniqueid") uniqueid: String,
-            @Param("limit") limit: Int,
-            @Param("offset") offset: Int
-    ): List<HistoriesEntity>
+@Repository
+interface HistoriesRepository : JpaRepository<HistoriesEntity, Long> {
+
+    fun findByIduserAndUniqueid(iduser: String, uniqueid: String): List<HistoriesEntity>
+
+    fun findByIduser(iduser: String, pageable: Pageable): List<HistoriesEntity>
 }
