d2eff8ae52046abfb2758dcc6bd3f22850288284
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-09-09 03:20:18 -0300

    add update method

--- src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/histories/controllers/HistoriesControllerV1.kt
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/histories/controllers/HistoriesControllerV1.kt
@@ -2,24 +2,28 @@ package br.com.lucascm.mangaeasy.micro_api_monolito.features.histories.controlle
 import br.com.lucascm.mangaeasy.micro_api_monolito.features.histories.repositories.HistoriesRepository
 import br.com.lucascm.mangaeasy.micro_api_monolito.core.entities.ResultEntity
 import br.com.lucascm.mangaeasy.micro_api_monolito.core.entities.StatusResultEnum
+import br.com.lucascm.mangaeasy.micro_api_monolito.core.service.GetUidByFeature
 import br.com.lucascm.mangaeasy.micro_api_monolito.core.service.HandleExceptions
 import br.com.lucascm.mangaeasy.micro_api_monolito.core.service.VerifyUserIdPermissionService
+import br.com.lucascm.mangaeasy.micro_api_monolito.features.histories.entities.HistoriesEntity
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.PageRequest
 import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 @RequestMapping("/v2/histories")
-class HistoriesControllerV1() {
+class HistoriesControllerV1 {
     @Autowired lateinit var repository: HistoriesRepository
     @Autowired lateinit var handleExceptions: HandleExceptions
     @Autowired lateinit var verifyUserIdPermissionService: VerifyUserIdPermissionService
-
     @GetMapping
     @ResponseBody
     fun list(
@@ -30,7 +34,7 @@ class HistoriesControllerV1() {
         authentication: Authentication
          ): ResultEntity {
         try {
-            verifyUserIdPermissionService.get(authentication, idUser);
+            verifyUserIdPermissionService.get(authentication, idUser)
             val result = if (uniqueId != null) {
                 repository.findByIduserAndUniqueid(iduser = idUser, uniqueid = uniqueId)
             } else {
@@ -44,6 +48,46 @@ class HistoriesControllerV1() {
                 status = StatusResultEnum.SUCCESS,
                 data = result,
                 message = "Listado com sucesso"
+            )
+        } catch (e: Exception) {
+            return handleExceptions.handleCatch(e)
+        }
+    }
+
+    @PutMapping
+    @ResponseBody
+    fun update(
+        @RequestBody body: HistoriesEntity,
+        authentication: Authentication
+    ) : ResultEntity {
+        try {
+            verifyUserIdPermissionService.get(authentication, body.iduser)
+            val result = repository.findByIduserAndUniqueid(
+              iduser = body.iduser,
+              uniqueid = body.uniqueid
+            )
+            if (result.isEmpty()) {
+                repository.save(body.copy(
+                    uid = GetUidByFeature().get("users-histories"),
+                    createdat = Date().time,
+                    updatedat = Date().time,
+                ))
+            } else {
+                val first = result.first()
+                repository.save(first.copy(updatedat = Date().time,
+                    capatual = body.capatual,
+                    chapterlidos = body.chapterlidos,
+                    currentchapter = body.currentchapter,
+                    isdeleted = body.isdeleted,
+                    issync = body.issync,
+                    manga = body.manga
+                ))
+            }
+            return ResultEntity(
+                total = 1,
+                status = StatusResultEnum.SUCCESS,
+                data = listOf(body),
+                message = "Adicionado com sucesso"
             )
         } catch (e: Exception) {
             return handleExceptions.handleCatch(e)
