39db7e776ba11d74a99825531e67ce8664955a79
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-09-09 01:56:18 -0300

    fix list method

--- src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/histories/controllers/HistoriesControllerV1.kt
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/histories/controllers/HistoriesControllerV1.kt
@@ -1,10 +1,12 @@
 package br.com.lucascm.mangaeasy.micro_api_monolito.features.histories.controllers
-
-import HistoriesRepository
+import br.com.lucascm.mangaeasy.micro_api_monolito.features.histories.repositories.HistoriesRepository
 import br.com.lucascm.mangaeasy.micro_api_monolito.core.entities.ResultEntity
 import br.com.lucascm.mangaeasy.micro_api_monolito.core.entities.StatusResultEnum
 import br.com.lucascm.mangaeasy.micro_api_monolito.core.service.HandleExceptions
+import br.com.lucascm.mangaeasy.micro_api_monolito.core.service.VerifyUserIdPermissionService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.PageRequest
+import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -13,24 +15,35 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/v2/histories")
-class HistoriesControllerV1(@Autowired val repository: HistoriesRepository) {
+class HistoriesControllerV1() {
+    @Autowired lateinit var repository: HistoriesRepository
     @Autowired lateinit var handleExceptions: HandleExceptions
+    @Autowired lateinit var verifyUserIdPermissionService: VerifyUserIdPermissionService
 
-    @GetMapping("/list")
+    @GetMapping
     @ResponseBody
     fun list(
-            @RequestParam limit: Int,
-            @RequestParam offset: Int,
-            @RequestParam idUser: String,
-            @RequestParam uniqueId: String,
-             ): ResultEntity {
+        @RequestParam idUser: String,
+        @RequestParam uniqueId: String?,
+        @RequestParam limit: Int?,
+        @RequestParam offset: Int?,
+        authentication: Authentication
+         ): ResultEntity {
         try {
-            val result =  repository.findHistoricoWithFilter(idUser,uniqueId,limit,offset)
+            verifyUserIdPermissionService.get(authentication, idUser);
+            val result = if (uniqueId != null) {
+                repository.findByIduserAndUniqueid(iduser = idUser, uniqueid = uniqueId)
+            } else {
+                repository.findByIduser(
+                    iduser = idUser,
+                    pageable = PageRequest.of(offset ?: 0, limit ?: 25)
+                )
+            }
             return ResultEntity(
-                    total =  result.size,
-                    status = StatusResultEnum.SUCCESS,
-                    data = result,
-                    message = "Listado com sucesso"
+                total =  result.size,
+                status = StatusResultEnum.SUCCESS,
+                data = result,
+                message = "Listado com sucesso"
             )
         } catch (e: Exception) {
             return handleExceptions.handleCatch(e)
