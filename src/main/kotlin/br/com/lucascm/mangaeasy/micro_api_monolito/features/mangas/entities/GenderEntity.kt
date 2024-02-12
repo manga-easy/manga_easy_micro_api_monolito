fb2def7c488b77cc62dc7ef864bda8657f771433
lucas <lucas.cardo123@hotmail.com>
2024-02-12 18:32:02 -0400

    remove href and state in gender imagechapter entities

--- src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/mangas/entities/GenderEntity.kt
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/mangas/entities/GenderEntity.kt
@@ -2,6 +2,5 @@ package br.com.lucascm.mangaeasy.micro_api_monolito.features.mangas.entities
 
 data class GenderEntity(
     val id: String?,
-    val title: String,
-    val href: String
+    val title: String
 )
