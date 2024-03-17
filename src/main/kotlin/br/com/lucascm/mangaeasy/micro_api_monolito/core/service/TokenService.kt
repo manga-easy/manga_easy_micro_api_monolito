e6a39e881d4bb33f575dc699a4adeb4c49c21768
lucas <lucas.cardo123@hotmail.com>
2024-03-17 17:49:42 -0400

    Update TokenService.kt

--- src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/core/service/TokenService.kt
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/core/service/TokenService.kt
@@ -1,21 +1,18 @@
 package br.com.lucascm.mangaeasy.micro_api_monolito.core.service
 
-import mu.KotlinLogging
 import org.springframework.security.oauth2.jwt.JwtDecoder
 import org.springframework.stereotype.Service
 
 @Service
-class TokenService(
-    private val jwtDecoder: JwtDecoder,
-    private val logger: KotlinLogging = KotlinLogging
-) {
+class TokenService(private val jwtDecoder: JwtDecoder) {
 
     fun parseToken(token: String?): String? {
         return try {
+            if (token == null) return null
+            if (token.isEmpty()) return null
             val jwt = jwtDecoder.decode(token)
            return  jwt.claims["userId"] as String?
         } catch (e: Exception) {
-            logger.logger("TokenService").error(e.message)
             null
         }
     }
