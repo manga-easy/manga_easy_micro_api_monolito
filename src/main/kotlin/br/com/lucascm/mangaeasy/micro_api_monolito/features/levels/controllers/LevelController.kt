b894489597f0d11ee30525ef4b17801c328d64fd
lucas <lucas.cardo123@hotmail.com>
2024-03-29 14:10:08 -0400

    add update total xp when earn xp

--- src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/levels/controllers/LevelController.kt
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/levels/controllers/LevelController.kt
@@ -9,6 +9,7 @@ import br.com.lucascm.mangaeasy.micro_api_monolito.features.levels.entities.earn
 import br.com.lucascm.mangaeasy.micro_api_monolito.features.levels.repositories.RankingCache
 import br.com.lucascm.mangaeasy.micro_api_monolito.features.levels.repositories.XpRepository
 import br.com.lucascm.mangaeasy.micro_api_monolito.features.mangas.repositories.CatalogRepository
+import br.com.lucascm.mangaeasy.micro_api_monolito.features.profile.repositories.ProfileRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
@@ -23,12 +24,19 @@ import kotlin.random.Random
 class LevelController {
     @Autowired
     lateinit var verifyUserIdPermissionService: VerifyUserIdPermissionService
+
     @Autowired
     lateinit var xpRepository: XpRepository
+
     @Autowired
     lateinit var catalogRepository: CatalogRepository
+
     @Autowired
     lateinit var rankingCache: RankingCache
+
+    @Autowired
+    lateinit var profileRepository: ProfileRepository
+
     @GetMapping("/{userID}")
     @ResponseBody
     fun getXp(authentication: Authentication, @PathVariable userID: String): ResultEntity {
@@ -36,46 +44,58 @@ class LevelController {
             verifyUserIdPermissionService.get(authentication, userID)
             val result = xpRepository.countXpTotalByUserId(userID)
             return ResultEntity(listOf(result ?: 0))
-        }catch (e: Exception){
+        } catch (e: Exception) {
             return HandleExceptions().handleCatch(e)
         }
     }
+
     @PutMapping("/{userID}/earn-xp")
     @ResponseBody
-    fun earnXp(authentication: Authentication,
-               @PathVariable userID: String,
-               @RequestBody body: earnXpDto
-    ): ResultEntity{
+    fun earnXp(
+        authentication: Authentication,
+        @PathVariable userID: String,
+        @RequestBody body: earnXpDto
+    ): ResultEntity {
         try {
             verifyUserIdPermissionService.get(authentication, userID)
             val manga = catalogRepository.findByUniqueid(body.uniqueID)
-            if (manga == null) throw BusinessException("Manga não catalogado")
+            if (manga == null) {
+                throw BusinessException("Manga não catalogado")
+            }
             val result = xpRepository.findByUserIDAndUniqueIDAndChapterNumber(
                 userID,
                 body.uniqueID,
                 body.chapterNumber
             )
-            if (result == null){
-                xpRepository.save(XpEntity(
-                    uniqueID = body.uniqueID,
-                    chapterNumber = body.chapterNumber,
-                    userID = userID,
-                    createdAt = Date().time,
-                    updatedAt = Date().time,
-                    quantity = Random.nextInt(1, 6).toLong(),
-                    totalMinutes = 1
-                ))
+            if (result == null) {
+                xpRepository.save(
+                    XpEntity(
+                        uniqueID = body.uniqueID,
+                        chapterNumber = body.chapterNumber,
+                        userID = userID,
+                        createdAt = Date().time,
+                        updatedAt = Date().time,
+                        quantity = Random.nextInt(1, 6).toLong(),
+                        totalMinutes = 1
+                    )
+                )
+                val profile = profileRepository.findByUserID(userID)
+                if (profile != null) {
+                    val totalXp = xpRepository.countXpTotalByUserId(userID)
+                    profileRepository.save(profile.copy(totalXp = totalXp!!))
+                }
                 return ResultEntity(listOf(true))
             }
             if (result.quantity > 50) throw BusinessException("Voçê ja atingiu o maximo de xp para esse capitulo do manga")
-            xpRepository.save(result.copy(
+            xpRepository.save(
+                result.copy(
                     updatedAt = Date().time,
                     totalMinutes = ++result.totalMinutes,
                     quantity = result.quantity + Random.nextInt(1, 6).toLong()
                 )
             )
             return ResultEntity(listOf(false))
-        }catch (e: Exception){
+        } catch (e: Exception) {
             return HandleExceptions().handleCatch(e)
         }
     }
@@ -89,7 +109,7 @@ class LevelController {
                     .withSort(Sort.by("place"))
             )
             return ResultEntity(result.toList())
-        }catch (e: Exception){
+        } catch (e: Exception) {
             return HandleExceptions().handleCatch(e)
         }
     }
@@ -101,7 +121,7 @@ class LevelController {
             val result = rankingCache.findById(userId)
             if (!result.isPresent) throw BusinessException("Ranking não encontrado ou em processamento")
             return ResultEntity(listOf(result))
-        }catch (e: Exception){
+        } catch (e: Exception) {
             return HandleExceptions().handleCatch(e)
         }
     }
