5ece5278ffe94ba616b11513abec972a7a8092b2
lucas <lucas.cardo123@hotmail.com>
2024-04-20 00:20:34 -0400

    add endpoint random

--- src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/mangas/controllers/CatalogController.kt
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/mangas/controllers/CatalogController.kt
@@ -2,6 +2,7 @@ package br.com.lucascm.mangaeasy.micro_api_monolito.features.mangas.controllers
 
 import br.com.lucascm.mangaeasy.micro_api_monolito.core.entities.ResultEntity
 import br.com.lucascm.mangaeasy.micro_api_monolito.core.entities.StatusResultEnum
+import br.com.lucascm.mangaeasy.micro_api_monolito.features.mangas.repositories.CatalogRepository
 import br.com.lucascm.mangaeasy.micro_api_monolito.features.mangas.services.CatalogService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
@@ -11,18 +12,23 @@ import org.springframework.web.bind.annotation.*
 class CatalogController {
     @Autowired
     lateinit var catalogService: CatalogService
+
+    @Autowired
+    lateinit var catalogRepository: CatalogRepository
+
     @GetMapping
     @ResponseBody
-    fun search(@RequestParam genres: String?,
-               @RequestParam search: String?,
-               @RequestParam author: String?,
-               @RequestParam page: Int?,
-               @RequestParam uniqueid: String?,
-               @RequestParam limit: Int?,
-               @RequestParam yearAt: Int?,
-               @RequestParam yearFrom: Int?,
-               @RequestParam scans: String?,
-               @RequestParam isAdult: Boolean?
+    fun search(
+        @RequestParam genres: String?,
+        @RequestParam search: String?,
+        @RequestParam author: String?,
+        @RequestParam page: Int?,
+        @RequestParam uniqueid: String?,
+        @RequestParam limit: Int?,
+        @RequestParam yearAt: Int?,
+        @RequestParam yearFrom: Int?,
+        @RequestParam scans: String?,
+        @RequestParam isAdult: Boolean?,
     ): ResultEntity {
         val result = catalogService.list(
             genres = genres?.split("<>") ?: listOf(),
@@ -37,25 +43,33 @@ class CatalogController {
             scans = scans,
         )
         return ResultEntity(
-           message = "Sucesso",
-           total = result.content.size,
-           data = result.content,
-           status = StatusResultEnum.SUCCESS,
+            message = "Sucesso",
+            total = result.content.size,
+            data = result.content,
+            status = StatusResultEnum.SUCCESS,
         )
     }
+
     @GetMapping("/over-18")
     @ResponseBody
     fun over18(): ResultEntity {
         val result = catalogService.list(
-                genres = listOf("adult"),
-                isAdult = true,
-                limit = 20000,
-            )
+            genres = listOf("adult"),
+            isAdult = true,
+            limit = 20000,
+        )
         return ResultEntity(
             message = "Sucesso",
             total = result.content.size,
             data = result.content.map { it.uniqueid }.toList(),
             status = StatusResultEnum.SUCCESS,
         )
+    }
+
+    @GetMapping("/random")
+    @ResponseBody
+    fun random(@RequestParam isAdult: Boolean?): ResultEntity {
+        val result = catalogRepository.findMangaRandom(isAdult ?: false)
+        return ResultEntity(listOf(result))
     }
 }
--- src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/mangas/repositories/CatalogRepository.kt
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/mangas/repositories/CatalogRepository.kt
@@ -8,11 +8,26 @@ import org.springframework.stereotype.Repository
 
 
 @Repository
-interface CatalogRepository : JpaSpecificationExecutor<CatalogEntity>, JpaRepository<CatalogEntity, Long>  {
+interface CatalogRepository : JpaSpecificationExecutor<CatalogEntity>, JpaRepository<CatalogEntity, Long> {
 
     fun findByUniqueid(uniqueid: String): CatalogEntity?
-    @Query("""
-        DELETE FROM `catalog` WHERE created_at = updated_at and created_at > UNIX_TIMESTAMP(NOW() - INTERVAL 60 DAY)
-    """, nativeQuery = true)
+
+    @Query(
+        """
+            DELETE FROM `catalog` WHERE created_at = updated_at and created_at > UNIX_TIMESTAMP(NOW() - INTERVAL 60 DAY)
+        """,
+        nativeQuery = true
+    )
     fun deleteMangaInactive()
+
+    @Query(
+        """
+            SELECT * FROM `catalog` c 
+            WHERE (genres NOT LIKE '%adult%' or ?1)
+            ORDER BY RAND()
+            LIMIT 1;
+        """,
+        nativeQuery = true
+    )
+    fun findMangaRandom(isAdult: Boolean): CatalogEntity
 }
