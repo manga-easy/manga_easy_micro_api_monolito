1f4cd392d5674cf38527ce2c17fe3e4fb95e1e7f
lucas <lucas.cardo123@hotmail.com>
2024-05-07 22:45:22 -0400

    add count total likes and views task

--- src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/mangas/repositories/LikeMangaRepository.kt
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/mangas/repositories/LikeMangaRepository.kt
@@ -2,7 +2,18 @@ package br.com.lucascm.mangaeasy.micro_api_monolito.features.mangas.repositories
 
 import br.com.lucascm.mangaeasy.micro_api_monolito.features.mangas.entities.LikeMangaEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface LikeMangaRepository : JpaRepository<LikeMangaEntity, Long> {
     fun findByUniqueidAndUserId(uniqueid: String, userId: String): LikeMangaEntity?
+
+    @Query(
+        """
+            SELECT COUNT(*)
+            FROM LikeMangaEntity dc 
+            WHERE dc.uniqueid = :uniqueid
+        """
+    )
+    fun countByUniqueid(@Param("uniqueid") uniqueid: String): Long
 }
--- src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/mangas/repositories/ViewMangaRepository.kt
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/mangas/repositories/ViewMangaRepository.kt
@@ -2,7 +2,18 @@ package br.com.lucascm.mangaeasy.micro_api_monolito.features.mangas.repositories
 
 import br.com.lucascm.mangaeasy.micro_api_monolito.features.mangas.entities.ViewMangaEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface ViewMangaRepository : JpaRepository<ViewMangaEntity, Long> {
     fun findByUniqueidAndUserId(uniqueid: String, userId: String): ViewMangaEntity?
+
+    @Query(
+        """
+            SELECT COUNT(*)
+            FROM ViewMangaEntity dc 
+            WHERE dc.uniqueid = :uniqueid
+        """
+    )
+    fun countByUniqueid(@Param("uniqueid") uniqueid: String): Long
 }
