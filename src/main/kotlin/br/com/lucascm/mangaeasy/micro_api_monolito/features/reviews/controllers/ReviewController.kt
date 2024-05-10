b9d4ef9a89e9b523ff6d2cead5f45a590f2ed6af
lucas <lucas.cardo123@hotmail.com>
2024-05-10 18:29:07 -0400

    implements reviews with cache

---
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/reviews/controllers/ReviewController.kt
@@ -0,0 +1,101 @@
+package br.com.lucascm.mangaeasy.micro_api_monolito.features.reviews.controllers
+
+import br.com.lucascm.mangaeasy.micro_api_monolito.core.entities.BusinessException
+import br.com.lucascm.mangaeasy.micro_api_monolito.core.entities.ResultEntity
+import br.com.lucascm.mangaeasy.micro_api_monolito.core.service.HandleExceptions
+import br.com.lucascm.mangaeasy.micro_api_monolito.core.service.HandleResponseApi
+import br.com.lucascm.mangaeasy.micro_api_monolito.features.mangas.repositories.CatalogRepository
+import br.com.lucascm.mangaeasy.micro_api_monolito.features.reviews.dtos.ReviewDto
+import br.com.lucascm.mangaeasy.micro_api_monolito.features.reviews.entities.ReviewMangaEntity
+import br.com.lucascm.mangaeasy.micro_api_monolito.features.reviews.repositories.ReviewRepository
+import br.com.lucascm.mangaeasy.micro_api_monolito.features.reviews.services.ReviewService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.Authentication
+import org.springframework.web.bind.annotation.*
+import java.util.*
+
+@RestController
+@RequestMapping("/review")
+class ReviewController {
+
+
+    @Autowired
+    lateinit var catalogRepository: CatalogRepository
+
+    @Autowired
+    lateinit var reviewRepository: ReviewRepository
+
+    @Autowired
+    lateinit var reviewService: ReviewService
+
+
+    @GetMapping("/v1/{uniqueid}")
+    fun list(@PathVariable uniqueid: String, @RequestParam page: Int?): ResponseEntity<Any> {
+        return try {
+            return HandleResponseApi().ok(reviewService.list(uniqueid, page))
+        } catch (e: Exception) {
+            HandleResponseApi().error(e)
+        }
+    }
+
+    @PostMapping("/v1/{uniqueid}")
+    @ResponseBody
+    fun create(
+        @RequestBody body: ReviewDto,
+        @PathVariable uniqueid: String,
+        authentication: Authentication,
+    ): ResultEntity {
+        return try {
+            val userId = authentication.principal.toString()
+            catalogRepository.findByUniqueid(uniqueid)
+                ?: throw BusinessException("Manga não encontrado: $uniqueid")
+            val review = reviewRepository.findByUniqueidAndUserId(uniqueid, userId)
+            if (review != null) {
+                throw BusinessException("Ja foi enviado: $uniqueid")
+            }
+            val result = reviewRepository.save(
+                ReviewMangaEntity(
+                    uniqueid = uniqueid,
+                    createdAt = Date().time,
+                    totalLikes = 0,
+                    userId = userId,
+                    updatedAt = Date().time,
+                    commentary = body.commentary,
+                    value = body.value,
+                    hasSpoiler = body.hasSpoiler,
+                    hasUpdated = false,
+                )
+            )
+            return ResultEntity(listOf(result))
+        } catch (e: Exception) {
+            HandleExceptions().handleCatch(e)
+        }
+    }
+
+    @PutMapping("/v1/{uniqueid}")
+    @ResponseBody
+    fun update(
+        @RequestBody body: ReviewDto,
+        @PathVariable uniqueid: String,
+        authentication: Authentication,
+    ): ResultEntity {
+        return try {
+            val userId = authentication.principal.toString()
+            val review = reviewRepository.findByUniqueidAndUserId(uniqueid, userId)
+                ?: throw BusinessException("Avaliação não encontrada: $uniqueid")
+            val result = reviewRepository.save(
+                review.copy(
+                    commentary = body.commentary,
+                    value = body.value,
+                    updatedAt = Date().time,
+                    hasSpoiler = body.hasSpoiler,
+                    hasUpdated = true,
+                )
+            )
+            return ResultEntity(listOf(result))
+        } catch (e: Exception) {
+            HandleExceptions().handleCatch(e)
+        }
+    }
+}
---
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/reviews/services/ReviewService.kt
@@ -0,0 +1,39 @@
+package br.com.lucascm.mangaeasy.micro_api_monolito.features.reviews.services
+
+import br.com.lucascm.mangaeasy.micro_api_monolito.features.profile.repositories.ProfileRepository
+import br.com.lucascm.mangaeasy.micro_api_monolito.features.reviews.dtos.ListReviewDto
+import br.com.lucascm.mangaeasy.micro_api_monolito.features.reviews.repositories.ReviewRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.data.domain.PageRequest
+import org.springframework.stereotype.Service
+
+@Service
+class ReviewService {
+    @Autowired
+    lateinit var reviewRepository: ReviewRepository
+
+    @Autowired
+    lateinit var profileRepository: ProfileRepository
+
+    @Autowired
+    lateinit var likeReviewRepository: ReviewRepository
+
+    @Cacheable("list-review")
+    fun list(uniqueid: String, page: Int?): List<ListReviewDto> {
+        val result = reviewRepository.findByUniqueid(uniqueid, PageRequest.of(page ?: 0, 25))
+        val list = mutableListOf<ListReviewDto>()
+        for (review in result) {
+            val profile = profileRepository.findByUserID(review.userId)
+            val totalLike = likeReviewRepository.countByUniqueid(review.uniqueid)
+            list.add(
+                ListReviewDto.fromEntity(
+                    review.copy(totalLikes = totalLike),
+                    profile?.name,
+                    profile?.picture
+                )
+            )
+        }
+        return list
+    }
+}
