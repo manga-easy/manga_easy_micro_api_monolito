5a42f14ed1bccb334f3c85757cf1fe17f00834f5
lucas <lucas.cardo123@hotmail.com>
2024-05-13 12:06:41 -0400

    update packages, kotlin and add package semver

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.0"
     id("io.sentry.jvm.gradle") version "4.3.0"
     kotlin("jvm") version "1.7.22"
-    kotlin("plugin.spring") version "1.7.22"
+    kotlin("plugin.spring") version "1.9.0"
 }
 group = "br.com.lucas-cm.manga-easy"
 version = "1.6.0"
@@ -28,26 +28,26 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.1")
-    implementation("org.mariadb.jdbc:mariadb-java-client:3.1.2")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.5")
     implementation("com.google.firebase:firebase-admin:9.2.0")
-    implementation("io.appwrite:sdk-for-kotlin:1.2.0")
+    implementation("io.appwrite:sdk-for-kotlin:2.0.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-    implementation("org.mariadb.jdbc:mariadb-java-client:3.1.2")
-    implementation("io.sentry:sentry-spring-boot-starter-jakarta:7.3.0")
-    implementation(enforcedPlatform("com.oracle.oci.sdk:oci-java-sdk-bom:3.36.0"))
+    implementation("org.mariadb.jdbc:mariadb-java-client:3.3.3")
+    implementation("io.sentry:sentry-spring-boot-starter-jakarta:7.8.0")
+    implementation(enforcedPlatform("com.oracle.oci.sdk:oci-java-sdk-bom:3.41.1"))
     implementation("com.oracle.oci.sdk:oci-java-sdk-common-httpclient-jersey3")
     implementation("com.oracle.oci.sdk:oci-java-sdk-addons-apache-configurator-jersey3")
     implementation("com.oracle.oci.sdk:oci-java-sdk-core")
     implementation("com.oracle.oci.sdk:oci-java-sdk-identity")
     implementation("com.oracle.oci.sdk:oci-java-sdk-objectstorage")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("net.swiftzer.semver:semver:2.0.0")
 }
 
 tasks.withType<KotlinCompile> {
