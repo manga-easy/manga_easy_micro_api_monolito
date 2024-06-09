b2cbf0937e77ef75017af17e9425f8e6d3cb9695
lucas <lucas.cardo123@hotmail.com>
2024-06-09 17:04:03 -0400

    fix cors

--- src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/core/configs/SecurityConfig.kt
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/core/configs/SecurityConfig.kt
@@ -24,9 +24,6 @@ import org.springframework.security.oauth2.server.resource.InvalidBearerTokenExc
 import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthenticationToken
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.stereotype.Component
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
@@ -57,25 +54,13 @@ class SecurityConfig(
         }
 
         // Other configuration
-        http.cors()
+        http.cors().disable()
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         http.csrf().disable()
         http.headers().frameOptions().disable()
         http.headers().xssProtection().disable()
 
         return http.build()
-    }
-
-    @Bean
-    fun corsConfigurationSource(): CorsConfigurationSource {
-        // allow localhost for dev purposes
-        val configuration = CorsConfiguration()
-        configuration.allowedOrigins = listOf("http://localhost:8080", "https://dash.mangaeasy.online")
-        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE")
-        configuration.allowedHeaders = listOf("authorization", "content-type")
-        val source = UrlBasedCorsConfigurationSource()
-        source.registerCorsConfiguration("/**", configuration)
-        return source
     }
 
     @Bean
