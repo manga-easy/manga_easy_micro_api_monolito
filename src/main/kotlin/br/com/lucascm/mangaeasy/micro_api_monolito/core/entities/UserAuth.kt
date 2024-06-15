6a7597126b748e52a6daf615c4dcdea06706557f
lucas <lucas.cardo123@hotmail.com>
2024-06-15 16:29:52 -0400

    add entity user auth in authentication

---
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/core/entities/UserAuth.kt
@@ -0,0 +1,5 @@
+package br.com.lucascm.mangaeasy.micro_api_monolito.core.entities
+
+data class UserAuth(
+    val userId: String
+)
--- src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/core/service/TokenService.kt
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/core/service/TokenService.kt
@@ -1,17 +1,17 @@
 package br.com.lucascm.mangaeasy.micro_api_monolito.core.service
 
+import br.com.lucascm.mangaeasy.micro_api_monolito.core.entities.UserAuth
 import org.springframework.security.oauth2.jwt.JwtDecoder
 import org.springframework.stereotype.Service
 
 @Service
 class TokenService(private val jwtDecoder: JwtDecoder) {
-
-    fun parseToken(token: String?): String? {
+    fun parseToken(token: String?): UserAuth? {
         return try {
             if (token == null) return null
             if (token.isEmpty()) return null
             val jwt = jwtDecoder.decode(token)
-           return  jwt.claims["userId"] as String?
+            return UserAuth(jwt.claims["userId"] as String)
         } catch (e: Exception) {
             null
         }
