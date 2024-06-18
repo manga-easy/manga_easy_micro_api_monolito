745a098c73f9bdc62995275b28eef0eb6b385247
lucas <lucas.cardo123@hotmail.com>
2024-06-17 21:26:37 -0400

    improvements in banner v2

--- src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/banners/controllers/BannersControllerV2.kt
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/banners/controllers/BannersControllerV2.kt
@@ -1,117 +1,82 @@
 package br.com.lucascm.mangaeasy.micro_api_monolito.features.banners.controllers
 
 import br.com.lucascm.mangaeasy.micro_api_monolito.core.entities.BusinessException
-import br.com.lucascm.mangaeasy.micro_api_monolito.core.entities.ResultEntity
-import br.com.lucascm.mangaeasy.micro_api_monolito.core.entities.StatusResultEnum
 import br.com.lucascm.mangaeasy.micro_api_monolito.core.entities.UserAuth
-import br.com.lucascm.mangaeasy.micro_api_monolito.core.service.GetUidByFeature
-import br.com.lucascm.mangaeasy.micro_api_monolito.core.service.HandleExceptions
 import br.com.lucascm.mangaeasy.micro_api_monolito.core.service.HandlerPermissionUser
+import br.com.lucascm.mangaeasy.micro_api_monolito.features.banners.dtos.CreateBannerDto
 import br.com.lucascm.mangaeasy.micro_api_monolito.features.banners.entities.BannersEntity
 import br.com.lucascm.mangaeasy.micro_api_monolito.features.banners.repositories.BannersRepository
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
-@RequestMapping("/v2/banners")
+@Tag(name = "Banners")
+@RequestMapping("/banners")
 class BannersControllerV2 {
     @Autowired
     lateinit var repository: BannersRepository
 
     @Autowired
-    lateinit var getIsUserAdmin: HandlerPermissionUser
+    lateinit var handlerPermissionUser: HandlerPermissionUser
 
-    @GetMapping("/list")
+    @GetMapping("/v1/list")
     fun list(
         @RequestParam status: String?,
         @RequestParam idhost: Int?
-    ): ResultEntity {
-        return try {
-            val result: List<BannersEntity> = repository.findAll()
-            ResultEntity(result)
-        } catch (e: Exception) {
-            HandleExceptions().handleCatch(e)
-        }
+    ): List<BannersEntity> {
+        return repository.findAll()
     }
 
-    @DeleteMapping("/{uid}")
+    @DeleteMapping("/v1/{uid}")
     fun delete(
         @AuthenticationPrincipal userAuth: UserAuth,
         @PathVariable uid: String
-    ): ResultEntity {
-        try {
-            getIsUserAdmin.handleIsAdmin(userAuth)
-
-            val result = repository.findByUid(uid)
-                ?: throw BusinessException("Banner não encontrado")
-
-            repository.delete(result)
-
-            return ResultEntity(
-                total = 0,
-                status = StatusResultEnum.SUCCESS,
-                data = emptyList(),
-                message = "Deletado com sucesso"
-            )
-        } catch (e: Exception) {
-            return HandleExceptions().handleCatch(e)
-        }
+    ) {
+        handlerPermissionUser.handleIsAdmin(userAuth)
+        val result = repository.findByUid(uid)
+            ?: throw BusinessException("Banner não encontrado")
+        repository.delete(result)
     }
 
-    @PostMapping
+    @PostMapping("/v1/")
     fun create(
         @AuthenticationPrincipal userAuth: UserAuth,
-        @RequestBody body: BannersEntity
-    ): ResultEntity {
-        try {
-            handleValidatorWrite(userAuth, body)
-            val result = repository.save(body.apply {
+        @RequestBody body: CreateBannerDto
+    ): BannersEntity {
+        handleValidatorWrite(userAuth, body)
+        return repository.save(
+            BannersEntity(
+                updatedat = Date().time,
+                link = body.link,
+                image = body.image,
                 createdat = Date().time
-                updatedat = Date().time
-                uid = GetUidByFeature().get("banners")
-            })
-            return ResultEntity(
-                total = 1,
-                status = StatusResultEnum.SUCCESS,
-                data = listOf(result),
-                message = "Criado com sucesso"
             )
-        } catch (e: Exception) {
-            return HandleExceptions().handleCatch(e)
-        }
+        )
     }
 
-    @PutMapping("/{uid}")
+    @PutMapping("/v1/{uid}")
     fun update(
         @AuthenticationPrincipal userAuth: UserAuth,
-        @RequestBody body: BannersEntity,
+        @RequestBody body: CreateBannerDto,
         @PathVariable uid: String
-    ): ResultEntity {
-        try {
-            handleValidatorWrite(userAuth, body)
-            val result = repository.findByUid(uid)
-                ?: throw BusinessException("Banner não encontrado")
-            val resultUpdate = result.apply {
-                updatedat = Date().time
-                link = body.link
-                image = body.image
-            }
-            repository.save(resultUpdate)
-            return ResultEntity(
-                total = 1,
-                status = StatusResultEnum.SUCCESS,
-                data = listOf(result),
-                message = "Alterado com sucesso"
-            )
-        } catch (e: Exception) {
-            return HandleExceptions().handleCatch(e)
+    ): BannersEntity {
+        handleValidatorWrite(userAuth, body)
+        val find = repository.findByUid(uid)
+            ?: throw BusinessException("Banner não encontrado")
+        val banner = find.apply {
+            updatedat = Date().time
+            link = body.link
+            image = body.image
         }
+        return repository.save(banner)
+
     }
 
-    fun handleValidatorWrite(@AuthenticationPrincipal userAuth: UserAuth, body: BannersEntity) {
-        getIsUserAdmin.handleIsAdmin(userAuth)
+    private fun handleValidatorWrite(@AuthenticationPrincipal userAuth: UserAuth, body: CreateBannerDto) {
+        handlerPermissionUser.handleIsAdmin(userAuth)
 
         if (body.image.isEmpty()) {
             throw BusinessException("Campo Image não pode ser vazio")
--- src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/banners/entities/BannersEntity.kt
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/banners/entities/BannersEntity.kt
@@ -1,6 +1,8 @@
 package br.com.lucascm.mangaeasy.micro_api_monolito.features.banners.entities
 
 import jakarta.persistence.*
+import org.hibernate.annotations.UuidGenerator
+
 @Entity
 @Table(name = "_1_database_1_collection_9")
 data class BannersEntity(
@@ -8,9 +10,9 @@ data class BannersEntity(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "_id")
     private var id: Long? = null,
-
+    @UuidGenerator
     @Column(name = "_uid")
-    var uid: String? = null,
+    var uid: String = "",
     var image: String = "",
     var link: String = "",
 
