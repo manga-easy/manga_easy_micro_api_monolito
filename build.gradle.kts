706522baa15376f895fef57d6c14641ee14f6670
lucas <lucas.cardo123@hotmail.com>
2024-06-20 22:26:26 -0400

    remove class unused

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect:2.0.0")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.3.0")
     implementation("com.google.firebase:firebase-admin:9.3.0")
-    implementation("io.appwrite:sdk-for-kotlin:5.0.2")
+    implementation("io.appwrite:sdk-for-kotlin:2.0.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test:3.2.6")
     implementation("org.springframework.boot:spring-boot-starter-web:3.2.6")
     implementation("org.springframework.boot:spring-boot-starter-security:3.2.6")
