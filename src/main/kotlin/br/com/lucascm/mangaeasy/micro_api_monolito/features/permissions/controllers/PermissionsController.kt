aecba49048511adf28459c23db507581f4002b15
lucas <lucas.cardo123@hotmail.com>
2024-07-13 16:24:13 -0400

    standardization permissions

--- src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/permissions/controllers/PermissionsController.kt
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/permissions/controllers/PermissionsController.kt
@@ -6,13 +6,16 @@ import br.com.lucascm.mangaeasy.micro_api_monolito.core.service.HandlerPermissio
 import br.com.lucascm.mangaeasy.micro_api_monolito.features.permissions.entities.CreatePermissionDto
 import br.com.lucascm.mangaeasy.micro_api_monolito.features.permissions.entities.PermissionsEntity
 import br.com.lucascm.mangaeasy.micro_api_monolito.features.permissions.repositories.PermissionsRepository
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 import java.util.*
+import kotlin.jvm.optionals.getOrNull
 
 @RestController
-@RequestMapping("/permission")
+@RequestMapping("/permissions")
+@Tag(name = "Permissions")
 class PermissionsController {
     @Autowired
     lateinit var repository: PermissionsRepository
@@ -24,6 +27,22 @@ class PermissionsController {
     fun list(@AuthenticationPrincipal userAuth: UserAuth): List<PermissionsEntity> {
         handlerPermissionUser.handleIsAdmin(userAuth)
         return repository.findAll()
+    }
+
+    @GetMapping("/v1/users/{userId}")
+    fun getByUserId(
+        @AuthenticationPrincipal userAuth: UserAuth,
+        @PathVariable userId: String
+    ): PermissionsEntity {
+        handlerPermissionUser.handleIsAdmin(userAuth)
+        return repository.findByUserId(userId)
+            ?: throw BusinessException("Permissão Não encontrado")
+    }
+
+    @GetMapping("/v1/{id}")
+    fun getById(@PathVariable id: String): PermissionsEntity {
+        return repository.findById(id).getOrNull()
+            ?: throw BusinessException("Permission não encontrado")
     }
 
     @PostMapping("/v1")
