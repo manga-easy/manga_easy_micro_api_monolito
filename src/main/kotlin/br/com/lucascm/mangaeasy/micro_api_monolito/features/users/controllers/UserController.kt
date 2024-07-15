7d4951c9b368871f06443381545d81a3bc48dafe
lucas <lucas.cardo123@hotmail.com>
2024-07-15 13:35:28 -0400

    standardization users

--- src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/users/controllers/UserController.kt
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/users/controllers/UserController.kt
@@ -1,54 +1,41 @@
 package br.com.lucascm.mangaeasy.micro_api_monolito.features.users.controllers
 
-import br.com.lucascm.mangaeasy.micro_api_monolito.core.entities.ResultEntity
-import br.com.lucascm.mangaeasy.micro_api_monolito.core.entities.StatusResultEnum
-import br.com.lucascm.mangaeasy.micro_api_monolito.core.service.HandleExceptions
+import br.com.lucascm.mangaeasy.micro_api_monolito.core.entities.UserAuth
+import br.com.lucascm.mangaeasy.micro_api_monolito.core.service.HandlerPermissionUser
 import br.com.lucascm.mangaeasy.micro_api_monolito.features.users.entities.UserEntity
 import br.com.lucascm.mangaeasy.micro_api_monolito.features.users.repositories.UserRepository
-import io.appwrite.Client
-import io.appwrite.extensions.toJson
-import io.appwrite.models.User
-import io.appwrite.services.Users
-import kotlinx.coroutines.runBlocking
-import okhttp3.internal.wait
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.core.env.Environment
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.servlet.function.ServerResponse.async
 
 @RestController
-@RequestMapping("/v1/users")
+@RequestMapping("/users")
+@Tag(name = "Users")
 class UserController {
-    @Autowired lateinit var repository: UserRepository
+    @Autowired
+    lateinit var repository: UserRepository
 
-    @GetMapping()
-    fun getUsers(@RequestParam search: String?) : ResultEntity {
-        try {
-            val result = repository.search(search)
-            return ResultEntity(
-                total = result.size,
-                status = StatusResultEnum.SUCCESS,
-                data = result,
-                message = "Listado com sucesso"
-            )
-        } catch (e: Exception) {
-            return HandleExceptions().handleCatch(e)
-        }
+    @Autowired
+    lateinit var handlerPermissionUser: HandlerPermissionUser
+
+    @GetMapping("/v1")
+    fun getUsers(
+        @RequestParam search: String?,
+        @AuthenticationPrincipal userAuth: UserAuth
+    ): List<UserEntity> {
+        handlerPermissionUser.handleIsAdmin(userAuth)
+        return repository.search(search)
+
     }
 
-    @GetMapping("/{userid}")
-    fun get(@PathVariable userid: String) : ResultEntity {
-        try {
-            val result = repository.search(userid)
-            return ResultEntity(
-                total = result.size,
-                status = StatusResultEnum.SUCCESS,
-                data = result,
-                message = "Listado com sucesso"
-            )
-        } catch (e: Exception) {
-            return HandleExceptions().handleCatch(e)
-        }
+    @GetMapping("/v1/{id}")
+    fun get(
+        @PathVariable id: String,
+        @AuthenticationPrincipal userAuth: UserAuth
+    ): UserEntity {
+        handlerPermissionUser.handleIsAdmin(userAuth)
+        return repository.getId(id)
     }
 
 }
