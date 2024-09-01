b2e2527c3610feff0cb323b0806a63fa7e46f85c
lucas <lucas.cardo123@hotmail.com>
2024-09-01 19:18:53 -0400

    add ReplaceWith in profile v1

--- src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/profile/entities/ProfileV1Entity.kt
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/profile/entities/ProfileV1Entity.kt
@@ -6,7 +6,10 @@ import jakarta.persistence.Id
 import org.bson.types.ObjectId
 import org.springframework.data.mongodb.core.mapping.Document
 
-@Deprecated("Use ProfileEntity, remover 0.18 -> 0.20")
+@Deprecated(
+    "Use ProfileEntity, remover 0.18 -> 0.20",
+    ReplaceWith("ProfileEntity")
+)
 @Document("profile")
 data class ProfileV1Entity(
     @Id
