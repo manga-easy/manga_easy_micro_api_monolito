af911b370fcbe76462322fee6b1b1b2bbd7c908b
lucas <lucas.cardo123@hotmail.com>
2024-09-13 11:34:21 -0400

    fix reviw cache

--- src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/reviews/dtos/ListReviewDto.kt
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/reviews/dtos/ListReviewDto.kt
@@ -3,7 +3,7 @@ package br.com.lucascm.mangaeasy.micro_api_monolito.features.reviews.dtos
 import br.com.lucascm.mangaeasy.micro_api_monolito.features.reviews.entities.ReviewEntity
 
 data class ListReviewDto(
-    val reviewEntity: ReviewEntity,
+    val review: ReviewEntity? = null,
     val userName: String? = null,
     var userImage: String? = null,
 )
