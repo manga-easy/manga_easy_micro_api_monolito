0538a9105f2d0b01d9900ed5abfbcf759d211be8
lucas <lucas.cardo123@hotmail.com>
2024-09-24 16:09:37 -0400

    add ignorer doc and rqueue in filter version app

---
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/core/configs/AppVersionFilter.kt
@@ -0,0 +1,46 @@
+package br.com.lucascm.mangaeasy.micro_api_monolito.core.configs
+
+import br.com.lucascm.mangaeasy.micro_api_monolito.core.service.toggle.ToggleEnum
+import br.com.lucascm.mangaeasy.micro_api_monolito.core.service.toggle.ToggleService
+import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.servlet.Filter
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletRequest
+import jakarta.servlet.ServletResponse
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import net.swiftzer.semver.SemVer
+import org.springframework.stereotype.Component
+
+
+@Component
+class AppVersionFilter : Filter {
+    val toggleService: ToggleService = ToggleService()
+    override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
+        val httpRequest = request as HttpServletRequest
+        val httpResponse = response as HttpServletResponse
+        val uri = httpRequest.requestURI
+        val headerVersion = httpRequest.getHeader("me-app-version")
+        val versionApp = SemVer.parse(headerVersion ?: "0.15.4")
+
+        // Ignora as URLs /doc/** e /rqueue/**
+        if (!uri.startsWith("/doc/") && !uri.startsWith("/rqueue/")) {
+            val minimumVersionApp = SemVer.parse(
+                toggleService.getToggle(ToggleEnum.minimalVersionApp)
+            )
+            // Verificar se a versão do aplicativo é muito antiga
+            if (versionApp < minimumVersionApp) {
+                val objectMapper = ObjectMapper()
+                val errorMessage =
+                    mapOf("message" to "A versão do aplicativo é muito antiga. Por favor, atualize para continuar.")
+                val jsonError = objectMapper.writeValueAsString(errorMessage)
+                httpResponse.contentType = "application/json"
+                httpResponse.status = 422
+                httpResponse.writer.write(jsonError)
+                return
+            }
+        }
+        // Continuar com o fluxo normal da requisição
+        chain.doFilter(request, response)
+    }
+}
--- src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/core/configs/SecurityConfig.kt
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/core/configs/SecurityConfig.kt
@@ -1,16 +1,6 @@
 package br.com.lucascm.mangaeasy.micro_api_monolito.core.configs
 
 import br.com.lucascm.mangaeasy.micro_api_monolito.core.service.TokenService
-import br.com.lucascm.mangaeasy.micro_api_monolito.core.service.toggle.ToggleEnum
-import br.com.lucascm.mangaeasy.micro_api_monolito.core.service.toggle.ToggleService
-import com.fasterxml.jackson.databind.ObjectMapper
-import jakarta.servlet.Filter
-import jakarta.servlet.FilterChain
-import jakarta.servlet.ServletRequest
-import jakarta.servlet.ServletResponse
-import jakarta.servlet.http.HttpServletRequest
-import jakarta.servlet.http.HttpServletResponse
-import net.swiftzer.semver.SemVer
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -23,7 +13,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.oauth2.server.resource.InvalidBearerTokenException
 import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthenticationToken
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.stereotype.Component
 import org.springframework.web.reactive.config.ResourceHandlerRegistry
 
 @Configuration
@@ -77,31 +66,5 @@ class SecurityConfig(
         if (!registry.hasMappingForPattern("/**")) {
             registry.addResourceHandler("/**").addResourceLocations("classpath:/public/")
         }
-    }
-}
-
-@Component
-class AppVersionFilter : Filter {
-    val toggleService: ToggleService = ToggleService()
-    override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
-        val httpRequest = request as HttpServletRequest
-        val httpResponse = response as HttpServletResponse
-        val headerVersion = httpRequest.getHeader("me-app-version")
-        val versionApp = SemVer.parse(headerVersion ?: "0.15.4")
-        val minimumVersionApp = SemVer.parse(toggleService.getToggle(ToggleEnum.minimalVersionApp))
-
-        // Verificar se a versão do aplicativo é muito antiga
-        if (versionApp < minimumVersionApp) {
-            val objectMapper = ObjectMapper()
-            val errorMessage =
-                mapOf("message" to "A versão do aplicativo é muito antiga. Por favor, atualize para continuar.")
-            val jsonError = objectMapper.writeValueAsString(errorMessage)
-            httpResponse.contentType = "application/json"
-            httpResponse.status = 422
-            httpResponse.writer.write(jsonError)
-            return
-        }
-        // Continuar com o fluxo normal da requisição
-        chain.doFilter(request, response)
     }
 }
