65b6366c4160e463afc32889c75f326e0909f57a
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2024-09-28 00:34:28 -0300

    Create ReleaseNotesRepository.kt

---
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/release_notes/repositories/ReleaseNotesRepository.kt
@@ -0,0 +1,12 @@
+package br.com.lucascm.mangaeasy.micro_api_monolito.features.release_notes.repositories
+
+import br.com.lucascm.mangaeasy.micro_api_monolito.features.release_notes.entities.ReleaseNotesEntity
+import org.springframework.data.domain.Page
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.domain.Pageable
+
+interface ReleaseNotesRepository : JpaRepository<ReleaseNotesEntity, String> {
+    fun findAllByOrderByCreatedAtDesc(pageable: Pageable): Page<ReleaseNotesEntity>
+    fun findFirstByOrderByCreatedAtDesc(): ReleaseNotesEntity?
+    fun findByVersion(version: String): ReleaseNotesEntity?
+}
