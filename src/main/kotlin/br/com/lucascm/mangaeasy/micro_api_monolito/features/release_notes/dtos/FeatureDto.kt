5c746835a391620d7d22b819441c633ea7f70c56
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2024-09-28 20:58:09 -0300

    remove feature and fix dtos

--- src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/release_notes/dtos/FeatureDto.kt
+++
@@ -1,7 +0,0 @@
-package br.com.lucascm.mangaeasy.micro_api_monolito.features.release_notes.dtos
-
-data class FeatureDto(
-    val title: String,
-    val subtitle: String?,
-    val description: String?
-)
--- src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/release_notes/dtos/FixDto.kt
+++
@@ -1,7 +0,0 @@
-package br.com.lucascm.mangaeasy.micro_api_monolito.features.release_notes.dtos
-
-data class FixDto(
-    val title: String,
-    val subtitle: String?,
-    val description: String?
-)
