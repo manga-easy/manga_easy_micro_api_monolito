1f6c3f0f022d119ac8d8ce7dd96d569a2cef0b80
lucas <lucas.cardo123@hotmail.com>
2024-10-07 11:43:05 -0400

    add new parameters in get hosts

--- src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/hosts/repositories/HostsRepository.kt
+++ src/main/kotlin/br/com/lucascm/mangaeasy/micro_api_monolito/features/hosts/repositories/HostsRepository.kt
@@ -2,13 +2,16 @@ package br.com.lucascm.mangaeasy.micro_api_monolito.features.hosts.repositories
 
 import br.com.lucascm.mangaeasy.micro_api_monolito.features.hosts.entities.HostsEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import org.springframework.stereotype.Repository
 
 @Repository
-interface HostsRepository : JpaRepository<HostsEntity, String> {
+interface HostsRepository :
+    JpaSpecificationExecutor<HostsEntity>,
+    JpaRepository<HostsEntity, String> {
     fun findByStatus(
         status: String
     ): List<HostsEntity>
 
-    fun findByHostId(idhost: Int): List<HostsEntity>
+    fun findByHostId(hostId: Int): List<HostsEntity>
 }
